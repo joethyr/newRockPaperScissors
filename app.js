f62a2540151b566d5e3f78d8dde7fb588cc0e847
joethyr <joethayr@gmail.com>
2020-12-09 20:52:55 -0700

    fixed reset button. Previously, the button was not working.  Added a function that stops playing the game whenever a score of 5 is reached.

--- app.js
+++ app.js
@@ -22,38 +22,53 @@ function computerPlay() {
 // computer randomly picks
 
 startGame();
+resetGame();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... Its a tie!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... The round's a tie!`;
   } else if (playerSelection == "rock" && computerSelection == "scissors") {
     playerScore.textContent = ++userWin;
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you win!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you win the round!`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     cpuScore.textContent = ++computerWin;
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you lose!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you lose the round!`;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore.textContent = ++userWin;
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you win!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you win the round!`;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     cpuScore.textContent = ++computerWin;
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you lose!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you lose the round!`;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore.textContent = ++userWin;
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you win!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you win the round!`;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     cpuScore.textContent = ++computerWin;
-    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you lose!`;
+    messageBox.textContent = `you chose and ${playerSelection} and the cpu chooses ${computerSelection}... you lose the round!`;
   } else {
     messageBox.textContent = `wrong input!`;
   }
+  checkGame();
 }
 // Function for gameplay single round.
+function checkGame() {
+  if (userWin == 5) {
+    buttonRow.forEach((input) => {
+      input.removeEventListener("click", getPlayerSelection);
+    });
+    messageBox.textContent = "CONGRATULATIONS! YOU WIN THE GAME!!";
+  } else if (computerWin == 5) {
+    buttonRow.forEach((input) => {
+      input.removeEventListener("click", getPlayerSelection);
+    });
+    messageBox.textContent = "TOO BAD! THE CPU WINS THE GAME!!";
+  }
+}
 
 // Execute the programs.
 
 function resetGame() {
-  resetBtn.addEventListener("click", () => location.reload());
+  reset.addEventListener("click", () => location.reload());
 }
 
 function startGame() {
